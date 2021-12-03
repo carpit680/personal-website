57a6e2cd54b0168b5eaf7133c484124a8fcf0455
carpit680 <carpit680@gmail.com>
2021-12-03 13:23:33 +0530

    msg on mobile devices

--- src/components/App.js
+++ src/components/App.js
@@ -13,39 +13,46 @@ import Blogs from "./pages/Blogs";
 import $ from "jquery";
 import jQuery from "jquery";
 
-
 function App() {
-	jQuery(document).ready(function () {
-		var mouseX = 0,
-			mouseY = 0;
-		var xp = 0,
-			yp = 0;
+	if ($(window).width() >= 815) {
+		jQuery(document).ready(function () {
+			var mouseX = 0,
+				mouseY = 0;
+			var xp = 0,
+				yp = 0;
 
-		$(document).mousemove(function (e) {
-			mouseX = e.pageX - 30;
-			mouseY = e.pageY - 30;
+			$(document).mousemove(function (e) {
+				mouseX = e.pageX - 30;
+				mouseY = e.pageY - 30;
+			});
+
+			setInterval(function () {
+				xp += (mouseX - xp) / 9;
+				yp += (mouseY - yp) / 9;
+				$("#circle").css({ left: xp + "px", top: yp + "px" });
+			}, 20);
 		});
-
-		setInterval(function () {
-			xp += (mouseX - xp) / 9;
-			yp += (mouseY - yp) / 9;
-			$("#circle").css({ left: xp + "px", top: yp + "px" });
-		}, 20);
-	});
-	return (
-		<Router>
-			{/* <Navbar /> */}
-			<span id='circle' class='circle'></span>
-			<Switch>
-				<Route path='/' exact component={Home} />
-				<Route path='/about' component={About} />
-				<Route path='/skills' component={Skills} />
-				<Route path='/work' component={Work} />
-				<Route path='/blogs' component={Blogs} />
-				<Route path='/contact' component={Contact} />
-			</Switch>
-		</Router>
-	);
+		return (
+			<Router>
+				{/* <Navbar /> */}
+				<span id='circle' class='circle'></span>
+				<Switch>
+					<Route path='/' exact component={Home} />
+					<Route path='/about' component={About} />
+					<Route path='/skills' component={Skills} />
+					<Route path='/work' component={Work} />
+					<Route path='/blogs' component={Blogs} />
+					<Route path='/contact' component={Contact} />
+				</Switch>
+			</Router>
+		);
+	} else {
+		return (
+			<div>
+				<h1 className="sorry">Sorry, this website is not supported on mobile devices yet.</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
